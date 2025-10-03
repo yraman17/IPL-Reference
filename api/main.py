f31e40c26157f4ba4e807c3db8b31a0139a6fa11
Yashas Raman <yraman@uci.edu>
2025-10-02 17:18:11 -0700

    Fixed minor errors

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from player_routes import router
+from .player_routes import router
 
 app = FastAPI()
 app.include_router(router)
